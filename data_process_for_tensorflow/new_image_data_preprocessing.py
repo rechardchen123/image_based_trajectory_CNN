78ed3dc0130d45e1937698474657b59c54638ccd
rechardchen123 <rechardchen123@outlook.com>
2018-12-23 23:13:38 +0000

    finish the function for transferring images and labels to tensors

--- data_process_for_tensorflow/new_image_data_preprocessing.py
+++ data_process_for_tensorflow/new_image_data_preprocessing.py
@@ -62,7 +62,38 @@ def get_files(file_dir,label_dir,ratio):
         item = labels[i]
         item = item.strip('\n')  # use strip() to delete the start and end string '\n'
         items = item.split(',')  # slice method using ','
-        image_name = items[0]
-        image_path = os.path.join(file_dir, image_name)
-        
+        #save the different motion mode into the different lists
+        if items[1]==1:
+            # it means the static/anchorage state for the vessel
+            label_static_state.append(int_to_one_hot(items[1:]))
+            image_name_static = items[0]
+            image_path_static = os.path.join(file_dir, image_name_static)
+            for file in os.listdir(image_path_static):
+                static_state.append(file)
+        elif items[2] == 1:
+            # it means the normal navigation state for vessel
+            label_normal_navigation.append(int_to_one_hot(items[1:])
+            image_name_normal_navigation = items[0]
+            image_path_normal_navigation = os.path.join(file_dir,image_name_normal_navigation)
+            for file in os.listdir(image_path_normal_navigation):
+                normal_navigation.append(file)
+        elif items[3] == 1:
+            # it means the maneuvring operation 
+            label_maneuvring_operation.append(int_to_one_hot(items[1:]))
+            image_name_maneuvring = items[0]
+            image_path_maneuvring = os.path.join(file_dir,image_name_maneuvring)
+            for file in os.listdir(image_path_maneuvring):
+                maneuvring_operation.append(file)
+    # step 2: hstack the label and image
+    image_list = np.hstack((static_state,normal_navigation,maneuvring_operation))
+    label_list = np.hstack((label_static_state,label_normal_navigation,label_maneuvring_operation))
+
+    # using shuffle
+    temp = np.array([image_list,label_list])
+    temp = np.transpose()
+    np.random.shuffle(temp)
+    # transfer all the img and label into list
+    all_image_list = list(temp[:,0])
+    all_label_list = list(temp[:, 1])
+    
 
