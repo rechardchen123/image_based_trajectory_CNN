120eb53a0ede442185f8f373665775fb2eb21c43
rechardchen123 <rechardchen123@outlook.com>
2019-01-03 11:47:39 +0000

    mini_revision_format

--- training_and_testing_processing_for_trajectory_classification_in_tensorflow/build_network.py
+++ training_and_testing_processing_for_trajectory_classification_in_tensorflow/build_network.py
@@ -8,7 +8,8 @@ import tensorflow as tf
 import numpy as np
 import os
 
-def convolution_layer(image_tensor,batch_size,n_classes):
+
+def convolution_layer(image_tensor, batch_size, n_classes):
     with tf.variable_scope('conv1') as scope:
         weights = tf.Variable(
             tf.truncated_normal(
@@ -21,8 +22,6 @@ def convolution_layer(image_tensor,batch_size,n_classes):
             dtype=tf.float32)
         conv = tf.nn.conv2d(
             image_tensor, weights, strides=[1, 1, 1, 1], padding='SAME')
-        shape1 = tf.shape(conv)
-        rank1 = tf.rank(conv)
         pre_activation = tf.nn.bias_add(conv, biases)
         conv1 = tf.nn.relu(pre_activation, name=scope.name)
         shape2 = tf.shape(conv1)
@@ -33,7 +32,6 @@ def convolution_layer(image_tensor,batch_size,n_classes):
             strides=[1, 2, 2, 1],
             padding='SAME',
             name='pooling1')
-        shape3 = tf.shape(pool1)
         norm1 = tf.nn.lrn(
             pool1,
             depth_radius=4,
@@ -41,7 +39,6 @@ def convolution_layer(image_tensor,batch_size,n_classes):
             alpha=0.001 / 9.0,
             beta=0.75,
             name='norm1')
-        shape4 = tf.shape(norm1)
     # convolutional layer 2
     # 3 X 3 convolutional kernals and total number 16
     with tf.variable_scope('conv2') as scope:
@@ -61,9 +58,7 @@ def convolution_layer(image_tensor,batch_size,n_classes):
             padding='SAME',
         )
         pre_activation = tf.nn.bias_add(conv, biases)
-        shape5 = tf.shape(pre_activation)
         conv2 = tf.nn.relu(pre_activation, name='conv2')
-        shape6 = tf.shape(conv2)
     with tf.variable_scope('pooling2_lrn') as scope:
         norm2 = tf.nn.lrn(
             conv2,
@@ -72,17 +67,14 @@ def convolution_layer(image_tensor,batch_size,n_classes):
             alpha=0.001 / 9.0,
             beta=0.75,
             name='norm2')
-        shape7 = tf.shape(norm2)
         pool2 = tf.nn.max_pool(
             norm2,
             ksize=[1, 3, 3, 1],
             strides=[1, 1, 1, 1],
             padding='SAME',
             name='pooling2')
-        shape8 = tf.shape(pool2)
     with tf.variable_scope('conv3') as scope:
         reshape = tf.reshape(pool2, shape=[batch_size, -1])
-        shape9 = tf.shape(reshape)
         dim = reshape.get_shape()[1].value
         weights = tf.Variable(
             tf.truncated_normal(
@@ -95,7 +87,6 @@ def convolution_layer(image_tensor,batch_size,n_classes):
             dtype=tf.float32)
         local3 = tf.nn.relu(
             tf.matmul(reshape, weights) + biases, name=scope.name)
-        shape10 = tf.shape(local3)
     # fully connected layer
     with tf.variable_scope('local4') as scope:
         weights = tf.Variable(
@@ -109,10 +100,8 @@ def convolution_layer(image_tensor,batch_size,n_classes):
             dtype=tf.float32)
         local4 = tf.nn.relu(
             tf.matmul(local3, weights) + biases, name=scope.name)
-        shape11 = tf.shape(local4)
     with tf.variable_scope('dropout') as scope:
         drop_out = tf.nn.dropout(local4, 0.8)
-        shape12 = tf.shape(drop_out)
     with tf.variable_scope('softmax_layer') as scope:
         weights = tf.Variable(
             tf.truncated_normal(
@@ -125,12 +114,12 @@ def convolution_layer(image_tensor,batch_size,n_classes):
             dtype=tf.float32)
         softmax_linear = tf.add(
             tf.matmul(local4, weights), biases, name='softmax_linear')
-        shape13 = tf.shape(softmax_linear)
         return softmax_linear
 
 
-def evaluation(softmax_linear, label1_tensor, label2_tensor, label3_tensor,learning_rate):
-    with tf.variable_scope('loss') as scope:
+def evaluation(softmax_linear, label1_tensor, label2_tensor, label3_tensor,
+               learning_rate):
+    with tf.name_scope('loss'):
         y1 = tf.nn.sigmoid(softmax_linear, name='label1_softmax')
         y2 = tf.nn.sigmoid(softmax_linear, name='label2_softmax')
         y3 = tf.nn.sigmoid(softmax_linear, name='label3_softmax')
@@ -140,9 +129,6 @@ def evaluation(softmax_linear, label1_tensor, label2_tensor, label3_tensor,learn
             y2, 1e-8, tf.reduce_max(y2), name='label2_softmax_clip')
         y3_1 = tf.clip_by_value(
             y3, 1e-8, tf.reduce_max(y3), name='label3_softmax_clip')
-        # loss1 = tf.reduce_mean(-tf.reduce_sum(label1_tensor*tf.log(y1_1)))
-        # loss2 = tf.reduce_mean(-tf.reduce_sum(label2_tensor*tf.log(y2_1)))
-        # loss3 = tf.reduce_mean(-tf.reduce_sum(label1_tensor*tf.log(y1_1)))
         cross_entropy1 = tf.nn.sigmoid_cross_entropy_with_logits(
             labels=label1_tensor, logits=y1_1, name='xentropy_per_label1')
         cross_entropy2 = tf.nn.sigmoid_cross_entropy_with_logits(
@@ -156,18 +142,22 @@ def evaluation(softmax_linear, label1_tensor, label2_tensor, label3_tensor,learn
         tf.summary.scalar('loss', loss)
 
         #optimization
-        train_op=tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss)
+        train_op = tf.train.AdamOptimizer(
+            learning_rate=learning_rate).minimize(loss)
 
-        with tf.variable_scope('accuracy') as scope:
-            correct_predict1 = tf.equal(tf.argmax(label1_tensor), tf.argmax(y1))
-            correct_predict2 = tf.equal(tf.argmax(label2_tensor), tf.argmax(y2))
-            correct_predict3 = tf.equal(tf.argmax(label3_tensor), tf.argmax(y3))
+        with tf.name_scope('accuracy'):
+            correct_predict1 = tf.equal(
+                tf.argmax(label1_tensor), tf.argmax(y1))
+            correct_predict2 = tf.equal(
+                tf.argmax(label2_tensor), tf.argmax(y2))
+            correct_predict3 = tf.equal(
+                tf.argmax(label3_tensor), tf.argmax(y3))
             correct_predict1 = tf.cast(correct_predict1, tf.float32)
             correct_predict2 = tf.cast(correct_predict2, tf.float32)
             correct_predict3 = tf.cast(correct_predict3, tf.float32)
-            accuracy1 = tf.reduce_mean(correct_predict1)
-            accuracy2 = tf.reduce_mean(correct_predict2)
-            accuracy3 = tf.reduce_mean(correct_predict3)
+            accuracy1 = tf.reduce_mean(correct_predict1,name='accuracy1')
+            accuracy2 = tf.reduce_mean(correct_predict2,name='accuracy2')
+            accuracy3 = tf.reduce_mean(correct_predict3,name='accuracy3')
             accuracy = (accuracy1 + accuracy2 + accuracy3) / 3
             tf.summary.scalar('accuracy', accuracy)
     return loss, accuracy
--- training_and_testing_processing_for_trajectory_classification_in_tensorflow/training_network.py
+++ training_and_testing_processing_for_trajectory_classification_in_tensorflow/training_network.py
@@ -26,8 +26,7 @@ def read_and_decode(record):
         shape=[3], dtype=tf.int64)
     save_image_label_dict['label_3'] = tf.FixedLenFeature(
         shape=[3], dtype=tf.int64)
-    parsed = tf.parse_single_example(
-        record, features=save_image_label_dict)
+    parsed = tf.parse_single_example(record, features=save_image_label_dict)
     image = tf.decode_raw(parsed['raw_image'], out_type=tf.uint8)
     image = tf.reshape(image, shape=[360, 490, 3])
     # standarization the image and accelerate the training process
@@ -36,9 +35,12 @@ def read_and_decode(record):
     label_1 = parsed['label_1']  # label1 for static or anchorage state
     label_2 = parsed['label_2']  # label2 for normal naivgation
     label_3 = parsed['label_3']  # label3 for maneuvring operation
-    label_1 = tf.cast(label_1, tf.int32)  # change the label1 data type into int32
-    label_2 = tf.cast(label_2, tf.int32)  # change the label2 data type into int32
-    label_3 = tf.cast(label_3, tf.int32)  # change the label3 data type into int32
+    label_1 = tf.cast(label_1,
+                      tf.int32)  # change the label1 data type into int32
+    label_2 = tf.cast(label_2,
+                      tf.int32)  # change the label2 data type into int32
+    label_3 = tf.cast(label_3,
+                      tf.int32)  # change the label3 data type into int32
     return image, label_1, label_2, label_3
 
 
@@ -52,7 +54,8 @@ with tf.name_scope('input_layer'):
     label3_tensor = tf.placeholder(
         dtype=tf.float32, shape=[BATCH_SIZE, 3], name='label3')
 
-    train_logit = build_network.convolution_layer(image_tensor, BATCH_SIZE, N_CLASSES)
+    train_logit = build_network.convolution_layer(image_tensor, BATCH_SIZE,
+                                                  N_CLASSES)
     train_evaluation = build_network.evaluation(train_logit, label1_tensor,
                                                 label2_tensor, label3_tensor,
                                                 LEARNING_RATE)
@@ -63,12 +66,14 @@ with tf.name_scope('input_layer'):
     #                                      y1, y2, y3)
 
     filenames = glob.glob(
-        r'C:\Users\LPT-ucesxc0\AIS-Data\AIS_trajectory_training_dataset\train.tfrecords')
+        r'C:\Users\LPT-ucesxc0\AIS-Data\AIS_trajectory_training_dataset\train.tfrecords'
+    )
     logs_train_dir = r'C:\Users\LPT-ucesxc0\AIS-Data\AIS_trajectory_training_dataset'
     train_dataset = tf.data.TFRecordDataset(filenames)
     train_dataset = train_dataset.map(read_and_decode)
     # batch_size_tensor  = tf.convert_to_tensor(BATCH_SIZE,tf.int64)
-    train_dataset = train_dataset.batch(batch_size=BATCH_SIZE, drop_remainder=True)
+    train_dataset = train_dataset.batch(
+        batch_size=BATCH_SIZE, drop_remainder=True)
     train_iter = train_dataset.make_one_shot_iterator()
     train_next_element = train_iter.get_next()
     init = tf.global_variables_initializer()
@@ -81,38 +86,40 @@ with tf.name_scope('input_layer'):
         try:
             while True:
                 image, label1, label2, label3 = session.run(train_next_element)
-            # _,tra_loss,tra_acc = session.run([train_op,train_loss,train_acc],
-            #                               feed_dict = {image_tensor:image,
-            #                                            label1_tensor:label1,
-            #                                            label2_tensor:label2,
-            #                                            label3_tensor:label3})
-                tra_loss,tra_acc = session.run([train_logit,train_evaluation],
-                                           feed_dict={  image_tensor:image,
-                                                        label1_tensor:label1,
-                                                        label2_tensor:label2,
-                                                        label3_tensor:label3})
-            # tra_logits = session.run(fetches=train_logit, feed_dict={image_tensor: image})
-            # tra_loss, y11, y22, y33 = session.run(fetches=[train_loss, y1, y2, y3],
-            #                                       feed_dict={image_tensor:image,
-            #                                                  train_logit: tra_logits,
-            #                                                  label1_tensor: label1,
-            #                                                  label2_tensor: label2,
-            #                                                  label3_tensor: label3})
-            # tra_op = session.run(fetches=train_op, feed_dict={train_loss: tra_loss})
-            # tra_acc = session.run(fetches=train_acc, feed_dict={label1_tensor: label1,
-            #                                                     label2_tensor: label2,
-            #                                                     label3_tensor: label3,
-            #                                                     y1: y11,
-            #                                                     y2: y22,
-            #                                                     y3: y33})
-                print('loss=',tra_loss)
-                print('accuracy=',tra_acc)
+                # _,tra_loss,tra_acc = session.run([train_op,train_loss,train_acc],
+                #                               feed_dict = {image_tensor:image,
+                #                                            label1_tensor:label1,
+                #                                            label2_tensor:label2,
+                #                                            label3_tensor:label3})
+                tra_loss, tra_acc = session.run(
+                    [train_logit, train_evaluation],
+                    feed_dict={
+                        image_tensor: image,
+                        label1_tensor: label1,
+                        label2_tensor: label2,
+                        label3_tensor: label3
+                    })
+                # tra_logits = session.run(fetches=train_logit, feed_dict={image_tensor: image})
+                # tra_loss, y11, y22, y33 = session.run(fetches=[train_loss, y1, y2, y3],
+                #                                       feed_dict={image_tensor:image,
+                #                                                  train_logit: tra_logits,
+                #                                                  label1_tensor: label1,
+                #                                                  label2_tensor: label2,
+                #                                                  label3_tensor: label3})
+                # tra_op = session.run(fetches=train_op, feed_dict={train_loss: tra_loss})
+                # tra_acc = session.run(fetches=train_acc, feed_dict={label1_tensor: label1,
+                #                                                     label2_tensor: label2,
+                #                                                     label3_tensor: label3,
+                #                                                     y1: y11,
+                #                                                     y2: y22,
+                #                                                     y3: y33})
+                print('loss=', tra_loss)
+                print('accuracy=', tra_acc)
                 # if count % 10 == 0:
                 #     summary_str = session.run(summary_op)
                 #     train_writer.add_summary(summary_str, step)
                 #     checkpoint_path = os.path.join(logs_train_dir, 'model.ckpt')
                 #     saver.save(session, checkpoint_path, global_step=step)
-            count +=1
+            count += 1
         except tf.errors.OutOfRangeError:
             print('end!')
-
